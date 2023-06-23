956d6e6afe0b959e9f2f70a4121b4cde1433ce34
“Demarcus <dcolemand@ymail.com>
2023-06-23 16:27:40 -0500

    Social Intro
    The intro was added

---
+++ social/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+
+
+
+
+function videoEnded() {
+    document.querySelector('.intro').classList.add('hidden');
+    document.querySelector('.start').classList.remove('hidden');
+}
