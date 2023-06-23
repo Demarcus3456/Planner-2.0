f424fe79422b8677974ce87f2c7ba70e25b2646c
“Demarcus <dcolemand@ymail.com>
2023-06-23 17:37:10 -0500

    social
    Added some content to social and week 1

--- social/script.js
+++ social/script.js
@@ -1,11 +1,17 @@
 'use strict'
 
+const start = document.querySelector('.start');
+const week1 = document.querySelector('.week1');
 
 
-
-
-
+// Shows strt button at end of video
 function videoEnded() {
     document.querySelector('.intro').classList.add('hidden');
-    document.querySelector('.start').classList.remove('hidden');
+    start.classList.remove('hidden');
 }
+
+// Show week 1 content
+start.addEventListener('click', function () {
+    start.classList.add('hidden');
+    week1.classList.remove('hidden');
+});
