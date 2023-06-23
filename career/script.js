35f2fe22495001aad89f2c0c3cf08ac7bfe2a058
“Demarcus <dcolemand@ymail.com>
2023-06-23 10:23:22 -0500

    Career-Js-Fun-Numbers
    Added the number information

--- career/script.js
+++ career/script.js
@@ -1,2 +1,16 @@
 "use strict";
 
+// Define your code example here
+var code = `
+// With decimals:
+let x1 = 34.00;
+
+// Without decimals:
+let x2 = 34;
+`;
+
+// Get the code example container element
+var codeExample = document.getElementById('numbers');
+
+// Set the code as the innerHTML of the container
+codeExample.innerHTML = code;
